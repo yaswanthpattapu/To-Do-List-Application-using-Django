a1259086ec5a05bc011c42407c2104a48b5c3f93
Yaswanth Kumar Pattapu <yaswanthpyk090@gmail.com>
2022-02-17 11:23:21 +0530

    Update urls.py

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path("api/", List),
+    path("", List),
     path("api/<int:id>/", Post_Single_List),
     path("add/", Post_List),
     path("update/<int:id>/", Update_List),
